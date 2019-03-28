fa1b9f856e5e6ab0601dbea44a9c3ce3ec1ee940
KevinWardStudent <kward11@student.fitchburgstate.edu>
2019-03-28 18:29:37 -0400

    Head Movement and Partial Body Movement added to Player Game Object

--- DayAtTheRange/Assets/RadioController.cs
+++ DayAtTheRange/Assets/RadioController.cs
@@ -1,56 +1,102 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RadioController : MonoBehaviour {
 
     // Declare Variables
     private int indexStation; // Value used to cycle between stations
     private bool consoleTextDisplayed; // Checked to see if console text is displayed for debugging
-    private int indexStationLimit;
-
-    //public GameObject station1;
-    //public GameObject station2;
-    //public GameObject station3;
 
     private AudioSource aud;
-    //public ArrayList audioTracks;
     public AudioClip[] arrayOfAudioTracks;
     public AudioClip station1;
     public AudioClip station2;
     public AudioClip station3;
 
+    public Text canActivateRadioText;
+    private bool canActivate;
+
+
 
 	// Use this for initialization
 	void Start ()
     {
-        aud = GetComponent<AudioSource>();
-        //arrayOfAudioTracks.Length += station1;
-            //.Add(station1);
+        consoleTextDisplayed = false; // Radio station has not been changed so, Console has not displayed text
+        aud = GetComponent<AudioSource>(); // Reference to Audio Source Component
+        arrayOfAudioTracks[0] = station1; // Set index value of Radio's track list postion 0 to first track
+        arrayOfAudioTracks[1] = station2; // Set index value of Radio's track list position 1 to second track
+        arrayOfAudioTracks[2] = station3; // Set index value of Radio's track list position 2 to third track
+
+        canActivate = false; // Player not in range of collider to active, so false
+        canActivateRadioText.text = ""; // Player not in range of collider, so nothing to display
 
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (indexStation == 1)
+        if (indexStation == 1 && consoleTextDisplayed == false)
         {
-            // Track 1
+            // Console Alert: Radio Station Set 1
+            Debug.Log("Now Playing Master Disorder");
+            // Play Track 1 "Master Disorder" and Stop the Other Tracks
+            aud.PlayOneShot(arrayOfAudioTracks[0]);
+            consoleTextDisplayed = true; // Radio station has been changed so, Console has displayed text
         }
 
-        if (indexStation == 2)
+        if (indexStation == 2 && consoleTextDisplayed == false)
         {
-            // Track 2
+            // Console Alert: Radio Station Set 2
+            Debug.Log("Now Playing Prelude and Action");
+            // Play Track 2 "Prelude and Action"
+            aud.PlayOneShot(arrayOfAudioTracks[1]);
+            consoleTextDisplayed = true; // Radio station has been changed so, Console has displayed text
         }
 
-        if (indexStation == 3)
+        if (indexStation == 3 && consoleTextDisplayed == false)
         {
-            // Track 3
+            // Console Alert: Radio Station Set 3
+            Debug.Log("Now Playing Ready Aim Fire");
+            // Play Track 3 "Ready Aim Fire"
+            aud.PlayOneShot(arrayOfAudioTracks[2]);
+            consoleTextDisplayed = true; // Radio station has been changed so, Console has displayed text
         }
 
-        if (indexStation > indexStationLimit)
+        if (indexStation > arrayOfAudioTracks.Length && consoleTextDisplayed == false)
         {
-            indexStation = 1;
+            // Console Alert: Radio Station Rset
+            Debug.Log("Radio Station Reset");
+            indexStation = 1; // Radio Station set back to one
+            consoleTextDisplayed = false; // Radio station has been changed so, Console has displayed text
+        }
+
+        // Below If Statement checks to see if fire button is presed
+        if (Input.GetKeyDown(KeyCode.F) && canActivate == true)
+        {
+            // Console Alert: Switch Station Button Pressed
+            Debug.Log("Switch Station Change Button Pressed");
+            aud.Stop(); // Stop Playing the Previous Track
+            indexStation++; // Then Incriment indexStation to change station track played
+            consoleTextDisplayed = false; // Radio station has been changed so, Console has displayed text
+
         }
 	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canActivate = true; // Player can activate object, now in range of collider
+            canActivateRadioText.text = "Press F to Activate Radio"; // Alert Player to the fact they can activate the Radio
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canActivate = false; // Player cannot activate object, not in range of collider
+            canActivateRadioText.text = ""; // Alert Player to the fact they cannot activate the Radio
+        }
+    }
 }
--- DayAtTheRange/Assets/TestPlayerMover.cs
+++ DayAtTheRange/Assets/TestPlayerMover.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestPlayerMover : MonoBehaviour {
+public class TestPlayerMover : MonoBehaviour
+{
 
     /*
      * This script's purpose is to simply move the Player Game Object on the X, Z plane 
@@ -11,20 +12,90 @@ public class TestPlayerMover : MonoBehaviour {
     // Declare Variables
     private Rigidbody rb; // References to Rigidbody Component
     public float movementSpeed; // Assigned in Unity Editor, Speed at which player moves
+    public float lookSpeed; // Assigned in Unity Editor, Speed at which player moves
 
-	// Use this for initialization
-	void Start ()
+    private HeadReference playerHead; // Grabs reference to transform head of Player
+    private GameObject playerHeadObject;
+
+    //Camera fpsCam;
+
+    float xAxisClamp;
+    
+
+    // Use this for initialization
+    void Start()
     {
-        rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        rb = GetComponent<Rigidbody>(); // Grabs reference to Player's Rigidbody component
+        Cursor.lockState = CursorLockMode.Locked; // Locks mouse to center of screen
+
+        /*
+        GameObject playerHeadObject = GameObject.FindWithTag("PlayerHead");
+        if (playerHeadObject != null)
+        {
+            playerHead = playerHeadObject.GetComponent<HeadReference>();
+        }
+        */
+        playerHeadObject = GameObject.Find("Player Head");
+
+        //fpsCam = GetComponent<Camera>();
+
+        xAxisClamp = 0.0F;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
+        // Player Movement
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movementInput = new Vector3(moveHorizontal, 0.0F, moveVertical);
         rb.velocity = movementInput * movementSpeed;
+
+        // Player Look
+
+        float lookHorizontal = Input.GetAxis("Mouse X");
+        float lookVertical = Input.GetAxis("Mouse Y");
+
+        //Vector3 lookInput = new Vector3(-lookVertical, lookHorizontal, 0.0F);
+        //transform.Rotate(-transform.right * lookVertical); // Rotates Torso up down
+        transform.Rotate(Vector3.up * lookHorizontal); // rotates Torso left right, but now apparently rotates head?
+        //transform.Rotate(transform.forward * lookHorizontal); //  rotates Torso in lean left right
+
+        playerHeadObject.transform.Rotate(Vector3.left * lookVertical); // Rotates Head up down
+        //playerHeadObject.transform.Rotate(transform.up * lookHorizontal); // Rotates  Head left right
+        //playerHeadObject.transform.rotation.y = transform.Rotate(transform.up * lookHorizontal);
+        //transform.Rotate(transform.up * lookVertical);
+
+
+        xAxisClamp += lookVertical;
+
+        if (xAxisClamp > 90.0F)
+        {
+            xAxisClamp = 90.0F;
+            lookVertical = 0.0F;
+            ClampXAxisRotationToValue(270.0F);
+
+        }
+        else if (xAxisClamp < -90.0F)
+        {
+            xAxisClamp = -90.0F;
+            lookVertical = 0.0F;
+            ClampXAxisRotationToValue(90.0F);
+        }
     }
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = playerHeadObject.transform.eulerAngles;
+        eulerRotation.x = value;
+        playerHeadObject.transform.eulerAngles = eulerRotation;
+    }
+    /*
+    void Shoot()
+    {
+        Camera fpsCam;
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 100F)) { }
+    }
+    */
 }
