ad9466066b4f4e05f7d52b64ac6367f62274ba9a
KevinWardStudent <kward11@student.fitchburgstate.edu>
2019-03-28 13:53:29 -0400

    Radio Object and Player Character Object

---
+++ DayAtTheRange/Assets/RadioController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioController : MonoBehaviour {
+
+    // Declare Variables
+    private int indexStation; // Value used to cycle between stations
+    private bool consoleTextDisplayed; // Checked to see if console text is displayed for debugging
+    private int indexStationLimit;
+
+    //public GameObject station1;
+    //public GameObject station2;
+    //public GameObject station3;
+
+    private AudioSource aud;
+    //public ArrayList audioTracks;
+    public AudioClip[] arrayOfAudioTracks;
+    public AudioClip station1;
+    public AudioClip station2;
+    public AudioClip station3;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        aud = GetComponent<AudioSource>();
+        //arrayOfAudioTracks.Length += station1;
+            //.Add(station1);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (indexStation == 1)
+        {
+            // Track 1
+        }
+
+        if (indexStation == 2)
+        {
+            // Track 2
+        }
+
+        if (indexStation == 3)
+        {
+            // Track 3
+        }
+
+        if (indexStation > indexStationLimit)
+        {
+            indexStation = 1;
+        }
+	}
+}
---
+++ DayAtTheRange/Assets/TestPlayerMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerMover : MonoBehaviour {
+
+    /*
+     * This script's purpose is to simply move the Player Game Object on the X, Z plane 
+     */
+
+    // Declare Variables
+    private Rigidbody rb; // References to Rigidbody Component
+    public float movementSpeed; // Assigned in Unity Editor, Speed at which player moves
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movementInput = new Vector3(moveHorizontal, 0.0F, moveVertical);
+        rb.velocity = movementInput * movementSpeed;
+    }
+}
