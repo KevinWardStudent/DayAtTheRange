9b9df51d80f572a2a19a675d5a66ec128a359349
KevinWardStudent <kward11@student.fitchburgstate.edu>
2019-04-12 12:00:42 -0400

    Pistol Animated
    Pistol Animated

---
+++ DayAtTheRange/Assets/PistolAnim.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolAnim : MonoBehaviour {
+    private Animator anim; // Reference to Animator component
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>(); // Grabs reference to Player's Animator component
+        anim.SetBool("AimDownSight", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Fire2") > 0)
+        {
+            anim.SetBool("AimDownSight", true);
+        }
+        else
+        {
+            anim.SetBool("AimDownSight", false);
+        }
+    }
+}
