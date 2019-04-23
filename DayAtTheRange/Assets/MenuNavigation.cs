5172412327909cf18dbe6a2ac2e658cde5dd37cf
KevinWardStudent <kward11@student.fitchburgstate.edu>
2019-04-23 13:22:16 -0400

    MenuNavigation Basic Setup and Tutorial
    MenuNavigation Basic Setup and Tutorial

---
+++ DayAtTheRange/Assets/MenuNavigation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour {
+
+    // first Add Scene Management
+    // Create public method, so that it can be accessed by multiple buttons
+    // For Modularity, the method will take a string argument for which scene we want to jump to
+    // Add Scene Manager, this takes either a string or int, 
+    // So now we need to attach this to some game object, create an empty
+    // Attach this script as a component to Menu Object
+    // Then in the button go to on click event
+    // it will change any variable, etc of any scripts, so attach menu object
+
+    // Tool Tip guide
+    /// <summary>
+    /// This script changes the level.
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ DayAtTheRange/Assets/Scenes/Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour {
+    /*
+     * There are two methods to doing menus. One is with a new scene another is with a game object which has buttons attached to it.
+     * UI elements are 2D. Try locking your view to 2D when editing the Canvas object or its elements. In the Tanks tutorial, there is a
+     * good description of how this all works. Couple set up things:
+     * 1. Set Game view to whatever your display will be for exported format.
+     * 2. Screenspace overlay for Huds, World Space is for Actual Text Elements
+     * 3. Canvas Scalar, always set the "Scale with Screen Size", you'll want to make your Reference Resolution to the size of your build's aspect/pixel ratio
+     * 4. You don't really want anything else different, now you have can place UI elements in a way, which makes more sense
+     * Keep in mind that your text has an invisble box around it to define the acceptable bounds of its placement.
+     * On to Buttons:
+     * Buttons are two objects. The button and the text.
+     * Anyway, it you want a pause menu you could do the same thing with a hide menu button and place the menu objects into a panel and then have that mapped to a keypress
+     * 
+     */
+}
