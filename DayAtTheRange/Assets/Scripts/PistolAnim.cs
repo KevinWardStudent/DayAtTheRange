91a0668dac5a8906dc3075c062880b6e317ff3fd
KevinWardStudent <kward11@student.fitchburgstate.edu>
2019-04-30 03:09:34 -0400

    betaModels of Wepaons imported
    Known Bug, Pistol slide rack does not slide back into position. This requires some rest and a rethinking of the code.

--- DayAtTheRange/Assets/Scripts/PistolAnim.cs
+++ DayAtTheRange/Assets/Scripts/PistolAnim.cs
@@ -9,11 +9,20 @@ public class PistolAnim : MonoBehaviour {
     {
         anim = GetComponent<Animator>(); // Grabs reference to Player's Animator component
         anim.SetBool("AimDownSight", false);
+        anim.SetBool("GunShot", false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            Debug.Log("Are you alive?");
+        }
+        else
+        {
+            //anim.SetBool("GunShot", false);
+        }
         if (Input.GetAxis("Fire2") > 0)
         {
             anim.SetBool("AimDownSight", true);
